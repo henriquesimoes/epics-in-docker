61b82a4673014ebe4baf1acfb786495c295885d3
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-06-07 17:51:52 -0300

    base: add Area Detector core modules.
    Include only core modules from Area Detector, which are required to
    build any other submodule.
    
    Git has been included in the build tools, since it allows to easily
    download the Area Detector modules in a single command in a parallel
    fashion. This makes easier to define the compatible versions as well,
    since it is defined at the supermodule level. For this configuration,
    the latest release is being used.
    
    Since area detector configuration is quite unique when compared to other
    EPICS modules, a new configuration script has been included for setting
    it up. This basically configures the top-level supermodule, which
    recursively compiles all modules listed in the `RELEASE.local` file.
    This configuration mostly follows the ones provided, and thus uses `sed`
    to edit the paths and the options instead of having the whole
    `configure` files written at once.
    
    Note that the IOCs inside the area detector modules have not been built,
    since this docker image serves for the purpose of being the support
    building the IOCs themselves in other images.
    
    Several libs from ADSupport has been already configured to be built (as
    it is the default value). The exception here is MagicGraphicK, because
    it is only required by ADFileMagick but requires some system libraries
    which aren't really needed to be handled for now.

---
+++ base/install_area_detector.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -ex
+
+git clone --depth 1 --branch ${AREA_DETECTOR_VERSION} \
+    https://github.com/areaDetector/areaDetector
+
+cd areaDetector
+
+git submodule update --init --depth 1 -j ${JOBS} \
+    ADSupport \
+    ADCore
+
+cd configure
+cat EXAMPLE_RELEASE.local | \
+    sed -e "s|^#ADCORE=|ADCORE=|" \
+        -e "s|^#ADSUPPORT=|ADSUPPORT=|" \
+    > RELEASE.local
+
+cat EXAMPLE_RELEASE_LIBS.local | \
+    sed -e "s|^SUPPORT=.*$|SUPPORT=${EPICS_MODULES_PATH}|" \
+        -e "s|^ASYN=.*$|ASYN=${EPICS_MODULES_PATH}/asyn|" \
+        -e "s|^AREA_DETECTOR=.*$|AREA_DETECTOR=${EPICS_MODULES_PATH}/areaDetector|" \
+        -e "s|^EPICS_BASE=.*$|EPICS_BASE=${EPICS_BASE_PATH}|" \
+    > RELEASE_LIBS.local
+
+cat EXAMPLE_RELEASE_PRODS.local | \
+    sed -e "s|^SUPPORT=.*$|SUPPORT=${EPICS_MODULES_PATH}|" \
+        -e "s|^ASYN=.*$|ASYN=${EPICS_MODULES_PATH}/asyn|" \
+        -e "s|^AREA_DETECTOR=.*$|AREA_DETECTOR=${EPICS_MODULES_PATH}/areaDetector|" \
+        -e "s|^AUTOSAVE=.*$|AUTOSAVE=${EPICS_MODULES_PATH}/autosave|" \
+        -e "s|^BUSY=.*$|BUSY=${EPICS_MODULES_PATH}/busy|" \
+        -e "s|^CALC=.*$|CALC=${EPICS_MODULES_PATH}/calc|" \
+        -e "s|^SSCAN=.*$|SSCAN=${EPICS_MODULES_PATH}/sscan|" \
+        -e "s|^DEVIOCSTATS=|#DEVIOCSTATS=|" \
+        -e "s|^SNCSEQ=|#SNCSEQ=|" \
+        -e "s|^EPICS_BASE=.*$|EPICS_BASE=${EPICS_BASE_PATH}|" \
+    > RELEASE_PRODS.local
+
+cat EXAMPLE_CONFIG_SITE.local | \
+    sed -e "s|^BUILD_IOCS\s*=.*$|BUILD_IOCS=NO|" \
+        -e "s|^WITH_GRAPHICSMAGICK\s*=.*$|WITH_GRAPHICSMAGICK=NO|" \
+    > CONFIG_SITE.local
+
+cd -
+
+make -j${JOBS}
+make clean
